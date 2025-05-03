427cbe8872ac22f154b2f57d6b9dc09977dcd73e
Abdelrhman(async) <talktoboda01@gmail.com>
2025-05-03 16:14:44 +0300

    Update main.ts

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,6 @@
 import { Wshandler } from "./app/ws-handler"
 function main (){
-    const option = {port:8080}
+    const option = {port:8081}
     const wsHandler = new Wshandler()
     wsHandler.intial(option)
 }
